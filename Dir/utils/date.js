31d9efd446e94568c3700efdb74aefecc531f4dc
luizcalvaosads <155118920+luizcalvaosads@users.noreply.github.com>
2024-04-13 10:55:54 -0300

    Add files via upload

---
+++ Dir/utils/date.js
@@ -0,0 +1,14 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});
+function getDateNow() {
+    var dataAtual = new Date();
+
+    var dia = dataAtual.getDate();
+    var mes = dataAtual.getMonth() + 1; // Os meses começam do zero, então adicionamos 1
+    var ano = dataAtual.getFullYear();
+
+    var dataHoraFormatada = dia + '/' + mes + '/' + ano;
+
+    return dataHoraFormatada;
+};
+
+exports.getDateNow = getDateNow;
---
+++ src/db/models/task.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose"; 
+
+const taskSchema = new Schema({
+    title: String,
+    description: String,
+    checked: Boolean,
+    priority: String, 
+    owner: String, 
+    date: String
+}); 
+
+const taskModel = model('task', taskSchema); 
+
+export { taskModel };
